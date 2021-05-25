5957514e91f79eda781420221dc3e7ff3ae69ad4
孙秋生 <634552451@qq.com>
2021-05-25 13:00:12 +0800

    add rust and csharp 27removeElemnet

---
+++ csharp/27removeElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class _27removeElement
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0, size = nums.Length;
+        for (int i = 0; i < size; ++i)
+        {
+            if (nums[i] != val)
+            {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+}
--- rust/algorithms.rs
+++ rust/algorithms.rs
@@ -27,3 +27,32 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
     return vec![0, 0];
 }
+
+
+/*
+示例 1：
+
+输入：nums = [3,2,2,3], val = 3
+输出：2, nums = [2,2]
+解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+示例 2：
+
+输入：nums = [0,1,2,2,3,0,4,2], val = 2
+输出：5, nums = [0,1,4,0,3]
+解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+
+*/
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut k: usize = 0;
+    let size = nums.len();
+    let mut i: usize = 0;
+    while i < size {
+        if nums[i] != val {
+            nums[k] = nums[i];
+            k += 1;
+        }
+        i += 1;
+    }
+
+    return k as i32;
+}
